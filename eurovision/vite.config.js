7ffdfbace8dd3ce66079fd1f7e0c0cd32a0836bf
Alejandro Rios <107919596+pikatostes@users.noreply.github.com>
2025-05-15 11:12:16 +0200

    Update vite.config.js

--- eurovision/vite.config.js
+++ eurovision/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Esto permite que se acceda desde fuera de la instancia (como tu navegador)
+    port: 5173,       // Puedes cambiar el puerto si lo prefieres, solo asegúrate de abrirlo en AWS
+  },
 })
-
